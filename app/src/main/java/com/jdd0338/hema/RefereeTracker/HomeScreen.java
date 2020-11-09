3b2d228e5845a903e825b58b74b83b258e1f944d
Christian Holmes <cholmes38@gatech.edu>
2020-11-09 15:52:09 -0500

    Project Init

---
+++ app/src/main/java/com/jdd0338/hema/RefereeTracker/HomeScreen.java
@@ -0,0 +1,13 @@
+package com.jdd0338.hema.RefereeTracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Home_Screen);
+    }
+}
