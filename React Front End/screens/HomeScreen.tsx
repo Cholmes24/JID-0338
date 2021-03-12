6c2a11ef53c11da9b8d755e16150e21721938e01
Tashfia Rahman <trahman34@gatech.edu>
2021-03-12 18:02:12 -0500

    Added button from home screen to most recent match

--- React Front End/screens/HomeScreen.tsx
+++ React Front End/screens/HomeScreen.tsx
@@ -4,26 +4,28 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import ScoreCounter from "../components/ScoreCounter"
 import { Text, View } from '../components/Themed';
+import { Button } from 'react-native-elements'
 import UserCard from "../components/UserCard"
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View style={styles.userCard}>
       <UserCard
         firstName={"longFirstName"}
         lastName={"longLastName"}
       />
+
+      <Button
+        buttonStyle={styles.matchButton}
+        title='Most Recent Match'
+      />
+      <View style={styles.filler}></View>
     </View>
+
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -33,4 +35,22 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  userCard: {
+    flex: 1,
+    paddingTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  matchButton: {
+    width: '100%',
+    padding: '10%',
+    marginTop: '5%',
+    marginBottom: '5%',
+    borderRadius: 15,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  filler: {
+    height: '40%'
+  }
 });
