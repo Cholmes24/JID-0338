f50108b1036e58083730c8ac23269e0fb95dbd1f
Victor Coleman <vcoleman9@gatech.edu>
2021-03-26 17:29:49 -0400

    standardize slices

--- React Front End/reducers/features/ScoringSlice.ts
+++ React Front End/reducers/features/ScoringSlice.ts
@@ -1,45 +1,31 @@
-import { ScoringActionType, IncreaseScoreAction, DecreaseScoreAction, IssueWarningAction, IssuePenaltyAction } from '../../redux-types/actionTypes'
-import { Scoring, Timer } from '../../redux-types/storeTypes'
+import { createSlice } from '@reduxjs/toolkit'
+import { Scoring } from '../../redux-types/storeTypes'
 
-export default function scoringReducer(state: Scoring, action: ScoringActionType) {
-  switch (action.type) {
-    case "INCREASE_SCORE":
-      return increaseScore(state, action)
-    case "DECREASE_SCORE":
-      return decreaseScore(state, action)
-    case "ISSUE_WARNING":
-      return issueWarning(state, action)
-    case "ISSUE_PENALTY":
-      return issuePenalty(state, action)
-    default:
-      return state
-  }
+const initialState: Scoring = {
+  points: 0,
+  numWarnings: 0,
+  numPenalties: 0,
 }
 
-function increaseScore(state: Scoring, action: IncreaseScoreAction): Scoring {
-  return {
-    ...state,
-    points: state.points + 1
+export const scoringSlice = createSlice({
+  name: "scoring",
+  initialState,
+  reducers: {
+    increaseScore: state => {
+      state.points += 1
+    },
+    decreaseScore: state => {
+      state.points = Math.max(state.points - 1, 0)
+    },
+    issueWarning: state => {
+      state.numWarnings += 1
+    },
+    issuePenalty: state => {
+      state.numPenalties += 1
+    }
   }
-}
+})
 
-function decreaseScore(state: Scoring, action: DecreaseScoreAction): Scoring {
-  return {
-    ...state,
-    points: Math.max(state.points - 1, 0)
-  }
-}
+export const { increaseScore, decreaseScore, issueWarning, issuePenalty } = scoringSlice.actions
 
-function issueWarning(state: Scoring, action: IssueWarningAction): Scoring {
-  return {
-    ...state,
-    numWarnings: state.numWarnings + 1
-  }
-}
-
-function issuePenalty(state: Scoring, action: IssuePenaltyAction): Scoring {
-  return {
-    ...state,
-    numPenalties: state.numPenalties + 1
-  }
-}
+export default scoringSlice.reducer
--- React Front End/reducers/features/TimerSlice.ts
+++ React Front End/reducers/features/TimerSlice.ts
@@ -1,73 +1,109 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { AddToTimerAction, TimerActionType, ToggleTimerAction, UpdateTimerAction } from '../../redux-types/actionTypes'
 import { Timer } from '../../redux-types/storeTypes'
 
-export default function timerReducer(state: Timer, action: TimerActionType) {
-  switch (action.type) {
-    case "TOGGLE_TIMER":
-      return toggleTimer(state, action)
-    case "ADD_TO_TIMER":
-      return addToTimer(state, action)
-    case "UPDATE_TIMER":
-      return updateTimer(state, action)
-    default:
-      return state
-  }
+const initialState: Timer = {
+  maxTime: 180000,
+  timeRemaining: 180000,
+  timeOfLastStart: 0,
+  timeRemainingAtLastStop: 180000,
+  isRunning: false
 }
 
-const calculateTimeRemaining = (
-  currentTime: number,
-  timeOfLastStart: number,
-  timeRemainingAtLastStop: number,
-  isRunning: boolean
-) => (
-  Math.max(0, timeRemainingAtLastStop - (isRunning ? (currentTime - timeOfLastStart) : 0))
+export const timerSlice = createSlice({
+  name: "timer",
+  initialState,
+  reducers: {
+    toggleTimer: (state, action: PayloadAction<number>) => {
+      if (state.timeRemaining > 0) {
+        if (state.isRunning) {
+          state.timeRemaining = calculateTimeRemaining(action.payload, state)
+          state.timeRemainingAtLastStop = state.timeRemaining
+        } else {
+          state.timeOfLastStart = action.payload
+        }
+        state.isRunning = !state.isRunning
+      }
+    },
+    addToTimer: (state, action: PayloadAction<number>) => {
+      if (state.timeRemaining <= 0) {
+        state.isRunning = false
+      } else {
+        state.timeRemainingAtLastStop += action.payload
+      }
+      state.timeRemaining += action.payload
+    },
+    updateTimer: (state, action: PayloadAction<number>) => {
+      state.timeRemaining = calculateTimeRemaining(action.payload, state)
+      if (state.timeRemaining <= 0) {
+        state.isRunning = false
+      }
+    }
+  }
+})
+
+export const { toggleTimer, addToTimer, updateTimer } = timerSlice.actions
+
+export default timerSlice.reducer
+
+// export default function timerReducer(state: Timer, action: TimerActionType) {
+//   switch (action.type) {
+//     case "TOGGLE_TIMER":
+//       return toggleTimer(state, action)
+//     case "ADD_TO_TIMER":
+//       return addToTimer(state, action)
+//     case "UPDATE_TIMER":
+//       return updateTimer(state, action)
+//     default:
+//       return state
+//   }
+// }
+
+const calculateTimeRemaining = (currentTime: number, state: Timer) => (
+  Math.max(0, state.timeRemainingAtLastStop - (state.isRunning ? (currentTime - state.timeOfLastStart) : 0))
 )
 
-function toggleTimer(state: Timer, action: ToggleTimerAction): Timer {
-  if (state.timeRemaining <= 0) {
-    return state
-  } else if (state.isRunning) {
-    const timeRemaining = calculateTimeRemaining(action.payload.currentTime,
-      state.timeOfLastStart, state.timeRemainingAtLastStop, state.isRunning
-    )
-    return {
-      ...state,
-      timeRemaining,
-      timeRemainingAtLastStop: timeRemaining,
-      isRunning: false
-    }
-  } else {
-    return {
-      ...state,
-      timeOfLastStart: action.payload.currentTime,
-      isRunning: true
-    }
-  }
-}
+// function toggleTimer(state: Timer, action: ToggleTimerAction): Timer {
+//   if (state.timeRemaining <= 0) {
+//     return state
+//   } else if (state.isRunning) {
+//     const timeRemaining = calculateTimeRemaining(action.payload.currentTime, state)
+//     return {
+//       ...state,
+//       timeRemaining,
+//       timeRemainingAtLastStop: timeRemaining,
+//       isRunning: false
+//     }
+//   } else {
+//     return {
+//       ...state,
+//       timeOfLastStart: action.payload.currentTime,
+//       isRunning: true
+//     }
+//   }
+// }
 
-function addToTimer(state: Timer, action: AddToTimerAction): Timer {
-  if (state.timeRemaining <= 0) {
-    return {
-      ...state,
-      isRunning: false,
-      timeRemaining: action.payload.amountToAdd
-    }
-  } else {
-    return {
-      ...state,
-      timeRemaining: state.timeRemaining + action.payload.amountToAdd,
-      timeRemainingAtLastStop: state.timeRemainingAtLastStop + action.payload.amountToAdd
-    }
-  }
-}
+// function addToTimer(state: Timer, action: AddToTimerAction): Timer {
+//   if (state.timeRemaining <= 0) {
+//     return {
+//       ...state,
+//       isRunning: false,
+//       timeRemaining: action.payload.amountToAdd
+//     }
+//   } else {
+//     return {
+//       ...state,
+//       timeRemaining: state.timeRemaining + action.payload.amountToAdd,
+//       timeRemainingAtLastStop: state.timeRemainingAtLastStop + action.payload.amountToAdd
+//     }
+//   }
+// }
 
-function updateTimer(state: Timer, action: UpdateTimerAction): Timer {
-  const timeRemaining = calculateTimeRemaining(action.payload.currentTime,
-    state.timeOfLastStart, state.timeRemainingAtLastStop, state.isRunning
-  )
-  return {
-    ...state,
-    timeRemaining,
-    isRunning: timeRemaining === 0 ? false : state.isRunning
-  }
-}
+// function updateTimer(state: Timer, action: UpdateTimerAction): Timer {
+//   const timeRemaining = calculateTimeRemaining(action.payload.currentTime, state)
+//   return {
+//     ...state,
+//     timeRemaining,
+//     isRunning: timeRemaining === 0 ? false : state.isRunning
+//   }
+// }
