5a888e198e3fd85a38396568af9dd127dc216289
Tashfia Rahman <trahman34@gatech.edu>
2021-04-01 12:44:36 -0400

    warning/penalties UI

--- React Front End/components/CompetitorColumn.tsx
+++ React Front End/components/CompetitorColumn.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import CustomButton from './Button'
 import { useDispatch, useSelector } from 'react-redux'
