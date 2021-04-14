815094f012bed9218a1c2e63bed2f94a46daa34e
Victor Coleman <vcoleman9@gatech.edu>
2021-04-14 17:58:26 -0400

    unstyled modal timer adding functionality

--- React Front End/components/Timer.tsx
+++ React Front End/components/Timer.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Text } from 'react-native-elements'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, TextInput, useColorScheme, Keyboard } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { RootType } from '../redux-types/storeTypes'
 import asMatchesAction from '../util/reduxActionWrapper'
 import { useAppDispatch, useAppSelector } from '../hooks/reduxHooks'
+import Modal from 'react-native-modal'
 
 type TimerProps = {
   matchId: number
@@ -16,12 +16,17 @@ export default function Timer({matchId}: TimerProps) {
   const dispatch = useAppDispatch()
   const matches = useAppSelector((state: RootType) => state.matches)
   const timerStore = matches.find(m => m.id === matchId)?.timer
+  const theme = useColorScheme()
 
   if (!timerStore) {
     throw Error("INVALID MATCH ID - TIMER NOT FOUND")
   }
 
+  const [ timeAddText, setTimeAddText ] = useState<string>('')
   const [ timeLeft, setTimeLeft ] = useState<number>(timerStore.timeRemaining)
+  const [ isModalVisible, setIsModalVisible ] = useState(false)
+
+  const toggleModal = () => setIsModalVisible(!isModalVisible)
 
   const refreshRate = 85 // in ms
   useEffect(() => {
@@ -69,8 +74,21 @@ export default function Timer({matchId}: TimerProps) {
   }
 
   const addTime = () => {
+    if (isValidDecimal(timeAddText.trim())) {
+      const asFloat: number = Number.parseFloat(timeAddText)
+      addParticularTime(asFloat * 1000)
+      setTimeAddText("")
+      setIsModalVisible(false)
+      Keyboard.dismiss()
+    }
+  }
+
+  const quickAddTime = () => {
+    addParticularTime(10000)
+  }
+
+  const addParticularTime = (amountToAdd: number) => {
     const currentTime = Date.now()
-    const amountToAdd = 10000
     setTimeLeft(timeLeft + amountToAdd)
     dispatch(
       asMatchesAction({
@@ -79,41 +97,179 @@ export default function Timer({matchId}: TimerProps) {
       }, matchId)
     )
   }
+
   const hasTimeLeft: () => boolean = () => timerStore.timeRemaining > 0
   const timerRunning: () => boolean = () => timerStore.isRunning
+  const onChangeText = (input: string) => setTimeAddText(input)
 
+  const styles = StyleSheet.create({
+    addTray: {
+      flexDirection: "row"
+    },
+    addBox: {
+      fontSize: 30,
+      backgroundColor: "#C0C0C0",
+      fontVariant: ['tabular-nums'],
+      margin: 10,
+      // alignSelf: "center",
+      flex: 1,
+      borderRadius: 8,
+    },
+    addText: {
+      fontSize: 30,
+      margin: 20,
+      flex: 1,
+    },
+    // modalFiller: {
+    //   flex: 1,
+    // },
+    modalButton: {
+      flex: 1
+    },
+    modalWindow: {
+      // height: 200,
+      backgroundColor: "white",
+      borderRadius: 8,
+
+      marginBottom: 150,
+      // justifyContent: 'center',
+      // alignSelf: 'center',
+      // alignItems: 'center',
+      // flex: 1,
+    },
+    text: {
+      fontSize: 30,
+      textAlign: 'center',
+      // fontFamily: Platform.OS === 'ios' ? 'Courier' : 'monospace'
+      fontVariant: ['tabular-nums'],
+      alignSelf: 'center',
+      marginHorizontal: 5,
+      ...testBorder
+    },
+    play_pause: {
+      ...buttonDefaults,
+      justifyContent: 'center',
+      alignSelf: 'center',
+      alignItems: 'center',
+      // shadowRadius: 4,
+      // shadowOffset: { width: 1, height: 3 },
+      // shadowColor: 'black',
+      // shadowOpacity: 0.4,
+    },
+    timerBox: {
+      width: '100%',
+      justifyContent: 'center',
+      alignSelf: 'center',
+      alignItems: 'center',
+    },
+    container: {
+      // width: '100%',
+      marginVertical: 3,
+      alignSelf: 'center',
+      flexDirection:'row',
+      alignContent: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+      ...testBorder
+    },
+    add_time: {
+      ...buttonDefaults,
+      justifyContent: 'center',
+      alignSelf: 'center',
+      // shadowRadius: 4,
+      // shadowOffset: { width: 1, height: 3 },
+      // shadowColor: 'black',
+      // shadowOpacity: 0.4,
+    },
+    modalAddTime: {
+      ...buttonDefaults,
+      justifyContent: 'center',
+      alignSelf: 'flex-end',
+      // marginVertical: 10,
+      // paddingLeft: 50,
+      marginBottom: 20,
+      // shadowRadius: 4,
+      // shadowOffset: { width: 1, height: 3 },
+      // shadowColor: 'black',
+      // shadowOpacity: 0.4,
+    }
+  });
   return (
-      <View style={styles.container}>
-        <Button
-          buttonStyle={styles.add_time}
-          icon={
-            <Icon
+    <View style={styles.container}>
+      {/* <Button
+        buttonStyle={styles.add_time}
+        disabled={!timerRunning()}
+        icon={
+          <Icon
+              name="plus"
+              size={40}
+              color="white"
+            />
+          }
+        onPress={addTime}
+      /> */}
+      <Button
+        buttonStyle={styles.add_time}
+        icon={
+          <Icon
+              name="plus"
+              size={40}
+              color="white"
+            />
+          }
+        onPress={toggleModal}
+      />
+      <Text style={styles.text}>{formattedTimeLeft()} </Text>
+      <Button
+        buttonStyle={styles.play_pause}
+        icon={timerRunning() && hasTimeLeft() ?
+          <Icon
+            name="pause"
+            size={40}
+            color="white"
+          />
+          :
+          <Icon
+            name="play"
+            size={40}
+            color="white"
+          />
+        }
+        onPress={() => (hasTimeLeft() ? toggle() : null)}
+      />
+      <Modal
+        isVisible={isModalVisible}
+        onBackdropPress={() => {setIsModalVisible(false); Keyboard.dismiss()}}
+        backdropOpacity={0.3}
+        animationIn="fadeInUp"
+        animationOut="fadeOutDown"
+        >
+        <View style={styles.modalWindow}>
+          <View style={styles.addTray}>
+            <Text style={styles.addText} >Seconds: </Text>
+            <TextInput
+              // placeholder="seconds"
+              style={styles.addBox}
+              onChangeText={onChangeText}
+              value={timeAddText}
+              keyboardType="numeric"
+              autoFocus={true}
+            />
+          </View>
+          <Button
+            buttonStyle={styles.modalAddTime}
+            icon={
+              <Icon
                 name="plus"
                 size={40}
                 color="white"
               />
             }
-          onPress={addTime}
-        />
-        <Text style={styles.text}>{formattedTimeLeft()} </Text>
-        <Button
-          buttonStyle={styles.play_pause}
-          icon={timerRunning() && hasTimeLeft() ?
-            <Icon
-              name="pause"
-              size={40}
-              color="white"
-            />
-            :
-            <Icon
-              name="play"
-              size={40}
-              color="white"
-            />
-          }
-          onPress={() => (hasTimeLeft() ? toggle() : null)}
-        />
-      </View>
+            onPress={addTime}
+          />
+        </View>
+      </Modal>
+    </View>
   )
 }
 const testBorder = {
@@ -129,49 +285,4 @@ const buttonDefaults = {
   ...testBorder
 }
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    textAlign: 'center',
-    // fontFamily: Platform.OS === 'ios' ? 'Courier' : 'monospace'
-    fontVariant: ['tabular-nums'],
-    alignSelf: 'center',
-    marginHorizontal: 5,
-    ...testBorder
-  },
-  play_pause: {
-    ...buttonDefaults,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
-    // shadowRadius: 4,
-    // shadowOffset: { width: 1, height: 3 },
-    // shadowColor: 'black',
-    // shadowOpacity: 0.4,
-  },
-  timerBox: {
-    width: '100%',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    // width: '100%',
-    marginVertical: 3,
-    alignSelf: 'center',
-    flexDirection:'row',
-    alignContent: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    ...testBorder
-  },
-  add_time: {
-    ...buttonDefaults,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    // shadowRadius: 4,
-    // shadowOffset: { width: 1, height: 3 },
-    // shadowColor: 'black',
-    // shadowOpacity: 0.4,
-  }
-});
+const isValidDecimal = (input: string) => (RegExp(/\d*((\.d+)|(\d\.?))/).test(input))
